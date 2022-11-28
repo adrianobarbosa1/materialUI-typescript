8199d3367b567a9f79d153c6394546519b9a265d
Adriano Barbosa <adriano.barbosa100@gmail.com>
2022-11-28 18:42:47 -0300

    feat: add theme claro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@mui/material"
 import { BrowserRouter } from "react-router-dom"
+import { LightTheme } from "./themes/Light"
 import { AppRoutes } from "./routes"
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
