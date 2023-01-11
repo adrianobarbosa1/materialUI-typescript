641f08b52fead7e9900003cf98aa65a777dba18d
Adriano Barbosa <adriano.barbosa100@gmail.com>
2023-01-11 15:13:17 -0300

    refactor texto explicativo

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -15,7 +15,7 @@ export const Environment = {
   LISTAGEM_VAZIA: "Nenhum registro encontrado",
 
   /**
-  * Url base de consultado dos dados dessa aplicação
+  * Url base de consulta dos dados dessa aplicação
   */
   URL_BASE: "http://localhost:3333"
 };
