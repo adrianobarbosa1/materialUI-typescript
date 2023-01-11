d77287f80a4d8c18a2b55f4b76b7a975869fc39e
Adriano Barbosa <adriano.barbosa100@gmail.com>
2023-01-11 15:14:34 -0300

    add routes cidades

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { Dashboard } from "../pages/dashboard/Dashboard";
 import { ListagemPessoas } from "../pages/pessoas/ListagemPessoas";
 import { DetalhePessoas } from "../pages/pessoas/DetalhePessoas";
+import { ListagemCidades } from "../pages/cidades/ListagemCidades";
+import { DetalheCidades } from "../pages/cidades/DetalheCidades";
 
 export const AppRoutes = () => {
   const { setDrawerOptions } = useDrawerContext();
@@ -11,16 +13,21 @@ export const AppRoutes = () => {
   useEffect(() => {
     setDrawerOptions([
       { path: "/paginainicial", icon: "home", label: "Página Inicial" },
+      { path: "/cidades", icon: "location_city", label: "Cidades" },
       { path: "/pessoas", icon: "people", label: "Pessoas" },
     ]);
   }, []);
 
   return (
     <Routes>
+      <Route path="/" />
       <Route path="/paginainicial" element={<Dashboard />} />
 
       <Route path="/pessoas" element={<ListagemPessoas />} />
       <Route path="/pessoas/detalhe/:id" element={<DetalhePessoas />} />
+
+      <Route path="/cidades" element={<ListagemCidades />} />
+      <Route path="/cidades/detalhe/:id" element={<DetalheCidades />} />
 
       <Route path="*" element={<Navigate to="/paginainicial" />} />
     </Routes>
