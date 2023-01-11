c260c1d5250e38cbda4f4870f416e647d2e784fd
Adriano Barbosa <adriano.barbosa100@gmail.com>
2023-01-11 15:12:18 -0300

    component unput TextField

---
+++ src/components/forms/UnTextField.tsx
@@ -0,0 +1,39 @@
+import { TextField, TextFieldProps } from "@mui/material";
+import { useField } from "@unform/core";
+import { useEffect, useState } from "react";
+
+type TCTextFieldProps = TextFieldProps & {
+  name: string;
+};
+
+export const UnTextField: React.FC<TCTextFieldProps> = ({ name, ...rest }) => {
+  const { fieldName, registerField, defaultValue, error, clearError } =
+    useField(name);
+  const [value, setValue] = useState(defaultValue || "");
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      getValue: () => value,
+      setValue: (_, newValue) => setValue(newValue),
+    });
+  }, [registerField, fieldName, value]);
+
+  return (
+    <TextField
+      {...rest}
+      error={!!error}
+      helperText={error}
+      defaultValue={defaultValue}
+      value={value}
+      onChange={(e) => {
+        setValue(e.target.value);
+        rest.onChange?.(e);
+      }}
+      onKeyDown={(e) => {
+        error && clearError();
+        rest.onKeyDown?.(e);
+      }}
+    />
+  );
+};
